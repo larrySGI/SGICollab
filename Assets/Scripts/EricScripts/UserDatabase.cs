5e318ecc88783fe6d7b919bbd6a4c3cf8c81f2e6
unknown <krislee@sgi-singapore.com>
2012-08-28 10:58:00 +0800

    new files from HengHong

--- Assets/Scripts/EricScripts/UserDatabase.cs
+++ Assets/Scripts/EricScripts/UserDatabase.cs
@@ -184,10 +184,7 @@ public class UserDatabase : MonoBehaviour {
 			Debug.Log(r.response.Text);
 			
 			if(r.response.Text == "user not signed in")
-			{
-				Destroy(GameObject.Find("Code"));
 				Application.LoadLevel(0);
-			}
 		}
 	}
 }
